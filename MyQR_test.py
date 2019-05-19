8a575b011e921642479661ab528ff2e74ee4b0b2
qiulongquan <qiulongquan@qiulongquannoMac-mini.local>
2019-05-19 16:07:02 +0900

    MyQR code

---
+++ MyQR_test.py
@@ -0,0 +1,8 @@
+from MyQR import myqr
+
+myqr.run(
+    words='https://github.com/qiulongquan',
+    picture='cat.gif',
+    colorized=True,
+    save_name='artisticQR_2.gif'
+)
---
+++ README.md
@@ -0,0 +1,2 @@
+制作普通二维码、带有图片的艺术二维码和动态二维码。
+https://www.shiyanlou.com/courses/1126/labs/7103/document/
